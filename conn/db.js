1c80f30d1b173074694a57a4eb3e6539314885a6
lokesh awasthi <awasthi21.lokesh@gmail.com>
2025-05-31 01:06:26 +0530

    commit backend

--- conn/db.js
+++ conn/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB connection successful"))
 .catch((error) => console.log("❌ Connection error: " + error.message));
 
